01edfd55525b3d3e3c4b3fb13b5ea433de096852
Tiantian828 <zhenghantian@163.com>
2024-11-19 20:14:42 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "var(--primary-color)",
-        secondary: "var(--secondary-color)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
